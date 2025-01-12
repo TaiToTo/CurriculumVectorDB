e2d946b086784901a4e4f466c3675715d0a5bfa3
YasuThompson <yasutotamura6161@gmail.com>
2025-01-12 16:03:24 +0100

    Seeded Estonian corpus as a demo db

--- SeedCurriculumVectorDB.py
+++ SeedCurriculumVectorDB.py
@@ -5,11 +5,26 @@ import weaviate
 from weaviate.classes.config import Configure
 
 if __name__=="__main__":
-    pdf_file_path_list = [
-        "curriculum_corpus/mathematics.pdf", 
-        "curriculum_corpus/science.pdf", 
-        "curriculum_corpus/social_studies.pdf"
-    ]
+    # pdf_file_path_list = [
+    #     "curriculum_corpus/mathematics.pdf", 
+    #     "curriculum_corpus/science.pdf", 
+    #     "curriculum_corpus/social_studies.pdf"
+    # ]
+
+    pdf_path_dict = {
+        "est_basic_school_nat_cur_2014_appendix_1_final.pdf": "language_and_literature", 
+        "est_basic_school_nat_cur_2014_appendix_2_final.pdf": "foreign_languages", 
+        "est_basic_school_nat_cur_2014_appendix_3_final.pdf": "mathematics", 
+        "est_basic_school_nat_cur_2014_appendix_4_final.pdf": "natural_science", 
+        "est_basic_school_nat_cur_2014_appendix_5_final.pdf": "social_studies", 
+        "est_basic_school_nat_cur_2014_appendix_6_final.pdf": "art", 
+        "est_basic_school_nat_cur_2014_appendix_7_final.pdf": "technology", 
+        "est_basic_school_nat_cur_2014_appendix_8_final.pdf": "physical_education", 
+        "est_basic_school_nat_cur_2014_appendix_9_final.pdf": "religious_studies", 
+        "est_basic_school_nat_cur_2014_appendix_10_final.pdf": "informatics", 
+        "est_basic_school_nat_cur_2014_appendix_11_final.pdf": "career_education", 
+        "est_basic_school_nat_cur_2014_appendix_12_final.pdf": "entrepreneurship_studies", 
+    }
 
     class_name = "CurriculumDemo"
 
@@ -33,8 +48,10 @@ if __name__=="__main__":
         )
 
     
-    for pdf_file_path in pdf_file_path_list:
+    base_dir_path = "curriculum_corpus/EstoniaBasicSchool"
 
+    for file_name, subject in pdf_path_dict.items():
+        pdf_file_path  =  os.path.join(base_dir_path, file_name)
         print("Seeding: " + pdf_file_path)
 
         with open(pdf_file_path, 'rb') as file:
@@ -48,7 +65,7 @@ if __name__=="__main__":
                 # Loop through each page and extract text
                 for page_number, page in enumerate(pdf_reader.pages):
                     batch.add_object({
-                        "page_number": int(page_number),
+                        "page_number": int(page_number + 1),
                         "text": page.extract_text(),
                         "subject": subject,
                     })
